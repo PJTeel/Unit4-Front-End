34d728b96980b1e39c8e3c8e4341c52aff5c0b0e
Paula Teel <teelpaulaj@johndeere.com>
2021-03-15 12:01:24 -0500

    added loop

--- src/App.js
+++ src/App.js
@@ -3,67 +3,56 @@ import React, { Component } from 'react';
 import { Route, Link, Switch, Redirect } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import axios from 'axios';
+import AllPaintings from './allPaintings';
+//import AllPaintins from './allPaintings.js'
 
 
 
 
 class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      paintings: []
+    }
+  }
   //method to call bakend to get data
   //console.log(data)
-
   componentDidMount = () => {
-    const url = 
+    const url = 'http://localhost:3001/paintings'
+    axios.get(url).then(response => {
+      console.log(response.data.allPaintings)
+      this.setState({
+        paintings: response.data.allPaintings
+      })
+    })
   }
 
   render() {
 
+    const paintings = this.state.paintings.map(painting => {
+      return (
+        <div className='gallery'>
+            <a target="_blank" href={painting.pic1}>
+              <img src={painting.pic1} alt="Farm House" />
+            </a>
+            <div className='desc'>{painting.title}</div>
+          </div>
+      )
+  });
 
-  return (
-    <div className="app">
+    return (
+      <div className="app">
         <header>
-        <h1>PTeel Originals</h1>
+          <h1>PTeel Originals</h1>
         </header>
-<div className='container'>     
-<div className='gallery'>
-  <a target="_blank" href="https://i.imgur.com/gd0YaAe.jpg">
-    <img src="https://i.imgur.com/gd0YaAe.jpg" alt="Farm House" />
-    </a>
-    <div className='desc'>Cobblestone farm house</div>
-</div>
+        <div className='container'>
+          {paintings}
+        </div>
+      </div>
 
-<div className='gallery'>
-  <a target="_blank" href="https://i.imgur.com/gd0YaAe.jpg">
-    <img src="https://i.imgur.com/gd0YaAe.jpg" alt="Farm House" />
-    </a>
-    <div class='desc'>Wanna play ball?</div>
-</div>
-
-<div className='gallery'>
-  <a target="_blank" href="https://i.imgur.com/gd0YaAe.jpg">
-    <img src="https://i.imgur.com/gd0YaAe.jpg" alt="Farm House" />
-    </a>
-    <div class='desc'>Hummingbird on coneflower</div>
-</div>
-
-<div className='gallery'>
-  <a target="_blank" href="https://i.imgur.com/gd0YaAe.jpg">
-    <img src="https://i.imgur.com/gd0YaAe.jpg" alt="Farm House" />
-    </a>
-    <div class='desc'>Lab pup with tennis ball</div>
-</div>
-
-<div className='gallery'>
-  <a target="_blank" href="https://i.imgur.com/gd0YaAe.jpg">
-    <img src="https://i.imgur.com/gd0YaAe.jpg" alt="Farm House" />
-    </a>
-    <div class='desc'>Owl</div>
-</div>
-
-</div> 
-    </div>
-
-  );
-}
+    );
+  }
 }
 
 export default App;
---
+++ src/allPaintings.js
@@ -0,0 +1,18 @@
+
+// import './App.css';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+
+// export default function AllPaintings(props) {
+//     const paintings = props.paintings.map(painting => {
+//         return <li key={allPaintings.id}><Link to={`/allPaintings/${allPaitings.id}`}>{allPaintings.title}</Link></li>
+//     });
+
+//     return (
+//         <section className='AllPaintings'>
+//             <hi>All paintings</hi>
+//             <ul>{paintings}</ul>
+
+//         </section>
+//     );
+// }
