eeda44ac4bd0abd5fdd48bd1cf6231cce5b0ad00
Paula Teel <teelpaulaj@johndeere.com>
2021-03-17 13:15:10 -0500

    Url change

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -25,6 +25,7 @@ class AboutMe extends Component {
                     <form>
                     <Contact/>
                     </form>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95561.6225862932!2d-90.5889795!3d41.541085949999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87e234eacdefe2cf%3A0x581e9972182bc4ad!2sSt.%20Ambrose%20University!5e0!3m2!1sen!2sus!4v1616003087567!5m2!1sen!2sus" width="400" height="300" loading="lazy"></iframe>
                 </main>
             //  </div>
       );
